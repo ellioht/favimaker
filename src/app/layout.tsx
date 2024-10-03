2b2ea214bd657a0a17a6568037c79df88b74c2ad
Elliot <130664947+ellioht@users.noreply.github.com>
2024-10-03 11:44:33 +0100

    add metadata and basic seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,26 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Favimaker",
-  description: "Create favicons and app icons for your website",
+  metadataBase: new URL("https://favimaker.com"),
+  keywords: ["favicon", "app icon", "generator", "creator", "free", "tool", "website", "web", "design", "maker", "favimaker"],
+  title: "Favimaker - Create Free Favicons & App Icons",
+  description: "Create favicons and app icons for your website. Free to use, no sign up required.",
+  openGraph: {
+    title: "Favimaker - Create Free Favicons & App Icons",
+    description: "Create favicons and app icons for your website. Free to use, no sign up required.",
+    type: "website",
+    locale: "en_GB",
+    url: "https://favimaker.com",
+    siteName: "Favimaker",
+    images: [
+      {
+        url: "https://www.favimaker.com/icon.png",
+        width: 1200,
+        height: 630,
+        alt: "Favimaker - Create Free Favicons & App Icons",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
